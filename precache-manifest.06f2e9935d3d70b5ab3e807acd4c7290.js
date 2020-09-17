2be0dfa59a7db3142c06dfa22052fbe582f13388
Pandimadhubabu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-17 12:30:09 +0000

    Deploying to master from  @ 0d2c503accd19c01b6c0312024ff5d9246b8a6e7 🚀

---
+++ precache-manifest.06f2e9935d3d70b5ab3e807acd4c7290.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0dd6b1d41742fd14885",
+    "url": "/static/css/main.d622c66f.chunk.css"
+  },
+  {
+    "revision": "b0dd6b1d41742fd14885",
+    "url": "/static/js/main.84addc8f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2a26c87962c93f5cc058",
+    "url": "/static/js/2.83d4ad58.chunk.js"
+  },
+  {
+    "revision": "2a6fb9299640c855e1c8c3d05dfe07e4",
+    "url": "/index.html"
+  }
+];
